f8994e99bba3d98705f6d768fabfe25974d6b6a5
Philip Jöbstl <philip.joebstl@hotmail.com>
2022-04-24 16:23:08 +0200

    Add  viper for configuration handling

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Printf("Hello %s", "world")
+	viper.SetConfigFile("config.yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("error while reading config file:\n", err)
+	}
+
+	src := viper.GetString("src")
+	if src == "" {
+		log.Fatalf("configuration error: src must not be empty")
+	}
+
+	dest := viper.GetString("dest")
+	if src == "" {
+		log.Fatalf("configuration error: dest must not be empty")
+	}
+
+	_, err = os.ReadDir(src)
+	if errors.Is(err, os.ErrNotExist) {
+		log.Fatalf("source directory does not exist: %s", src)
+	} else if err != nil {
+		log.Fatalf("error while reading source directory: %w", err)
+	}
+
+	_, err = os.ReadDir(dest)
+	if errors.Is(err, os.ErrNotExist) {
+		log.Fatalf("destination directory does not exist: %s", dest)
+	} else if err != nil {
+		log.Fatalf("error while reading destination directory: %w", err)
+	}
 }
